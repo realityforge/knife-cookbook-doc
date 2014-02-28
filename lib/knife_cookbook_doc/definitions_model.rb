524070c46f474293404be96c31f2508315f50575
zhelyan.panchev <jelian@gmail.com>
2014-02-28 18:01:39 +0000

    extract definition parameters and combine with user added descriptions if specified

--- lib/knife_cookbook_doc/definitions_model.rb
+++ lib/knife_cookbook_doc/definitions_model.rb
@@ -20,13 +20,30 @@ module KnifeCookbookDoc
       current_section = 'main'
       description.each_line do |line|
         if /^ *\@param *([^ ]*) (.*)$/ =~ line
-          params[$1] = $2.strip
+          params[$1] = {}
+          params[$1]['descr'] = $2.strip
         elsif /^ *\@section (.*)$/ =~ line
           current_section = $1.strip
         else
           lines = (top_level_descriptions[current_section] || [])
-          lines << line.gsub("\n",'')
+          lines << line.gsub("\n", '')
           top_level_descriptions[current_section] = lines
+        end
+      end
+      load_properties
+    end
+
+    def load_properties
+      code = IO.read(@filename)
+      code.gsub(/^ *define (.*?) (?=do)/m) do
+        all = $1.split(' ', 2)
+        @name = all.shift.gsub(/:/, '')
+        next if all.empty?
+        all = eval("{#{all.last}}") rescue {}
+        all.each do |k, v|
+          p_name = k.to_s
+          params[p_name] ||= {}
+          params[p_name]['default'] = v ? v : 'nil'
         end
       end
     end
