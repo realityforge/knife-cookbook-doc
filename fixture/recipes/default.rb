2e200d47c70e9906da7764c380324126cde5332c
Peter Donald <peter@realityforge.org>
2015-08-06 10:24:16 +1000

    Cleanup whitespace issues

--- fixture/recipes/default.rb
+++ fixture/recipes/default.rb
@@ -14,4 +14,3 @@ The recipe is awesome. It does thing 1, thing 2 and thing 3!
 #<> MyApp Admin Group: The group allowed to manage MyApp.
 
 # <> MyApp Admin Group: The group allowed to manage MyApp.
-
