7aaeee15ccb9ed79ef8bcaf3e1e1f7bb6420734a
Peter Donald <peter@realityforge.org>
2015-08-06 10:27:27 +1000

    Remove accidently duplicated line

--- fixture/recipes/default.rb
+++ fixture/recipes/default.rb
@@ -12,5 +12,3 @@ The recipe is awesome. It does thing 1, thing 2 and thing 3!
 =end
 
 #<> MyApp Admin Group: The group allowed to manage MyApp.
-
-# <> MyApp Admin Group: The group allowed to manage MyApp.
